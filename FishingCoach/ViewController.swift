4f9e608bc8d065715763d155e2a133a7d2bf45e1
Alexis PAQUIN <chpaqou@gmail.com>
2019-11-17 11:09:33 +0100

    Commit 19-11-17 FIRST COMMIT

---
+++ FishingCoach/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FishingCoach
+//
+//  Created by JEAN NEYMAR on 17/11/2019.
+//  Copyright © 2019 JEAN NEYMAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
